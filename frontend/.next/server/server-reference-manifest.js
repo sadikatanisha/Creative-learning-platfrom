a21e1884a9633e0583228453bf5bbec9a026ddcc
sadikatanisha <sadikatanisha@gmail.com>
2024-05-17 14:57:12 +0600

    initial commit

---
+++ frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IJOE3/1oLglv3s+Z8gNQ1m4925KPgWEciP/x9BaRL3Y=\"\n}"
---
+++ frontend/redux/features/api/apiSlice.ts
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { RootState } from "./types"; // adjust the import path as necessary
+import { userLoggedIn, userLoggedOut } from "../auth/authSlice";
+export interface AuthState {
+  user: string | null; // Adjust as needed
+  accessToken: string | null;
+}
+export interface RootState {
+  auth: AuthState;
+}
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8088/api/v1/",
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+      const token = state.auth.accessToken;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    refreshToken: builder.query({
+      query: () => ({
+        url: "refresh",
+        method: "GET",
+        credentials: "include",
+      }),
+    }),
+    loadUser: builder.query({
+      query: () => ({
+        url: "profile",
+        method: "GET",
+        credentials: "include",
+      }),
+      async onQueryStarted(args, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error: any) {
+          console.log(error.message);
+          if (error.status === 401) {
+            dispatch(userLoggedOut());
+          }
+        }
+      },
+    }),
+  }),
+});
+
+export const { useRefreshTokenQuery, useLoadUserQuery } = apiSlice;
